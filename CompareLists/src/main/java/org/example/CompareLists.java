0d150c0bb558ad2e84671e5d152671fa8b768c76
BrightSparkle <ekaterinamrch@gmail.com>
2025-04-27 17:45:30 +0300

    Create class CompareLists

---
+++ CompareLists/src/main/java/org/example/CompareLists.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CompareLists {
+
+    private static final int ITERATIONS = 10_000;
+
+
+}
