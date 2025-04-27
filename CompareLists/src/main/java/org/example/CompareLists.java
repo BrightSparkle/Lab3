2b3f62c38593a9bdbb1ad510ac50d5dd60c86f7f
BrightSparkle <ekaterinamrch@gmail.com>
2025-04-27 18:13:11 +0300

    Create testOperation

--- CompareLists/src/main/java/org/example/CompareLists.java
+++ CompareLists/src/main/java/org/example/CompareLists.java
@@ -7,6 +7,26 @@ public class CompareLists {
 
     private static final int ITERATIONS = 10_000;
 
+    private static void testOperation(String operation, List<Integer> list, int iterations) {
+        String listType = list instanceof ArrayList ? "ArrayList" : "LinkedList";
+        long time = 0;
+
+        switch (operation) {
+            case "add":
+                time = testAdd(list, iterations);
+                break;
+            case "delete":
+                time = testDelete(list, iterations);
+                break;
+            case "get":
+                time = testGet(list, iterations);
+                break;
+        }
+
+        System.out.printf("| %-7s | %-14s | %-8d | %-16d |\n",
+                operation, listType, iterations, time);
+    }
+
     private static long testAdd(List<Integer> list, int iterations) {
         long start = System.nanoTime();
         for (int i = 0; i < iterations; i++) {
